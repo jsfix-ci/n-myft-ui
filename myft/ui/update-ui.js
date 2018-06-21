19b70797ffb69277f57767bde26975770111483c
chee <chee@snake.dog>
2018-06-21 15:25:44 +0100

    set tokens on context element when updating ui
     🐿 v2.9.0

--- myft/ui/update-ui.js
+++ myft/ui/update-ui.js
@@ -2,6 +2,7 @@ import personaliseLinks from './personalise-links';
 import * as loadedRelationships from './lib/loaded-relationships';
 import * as buttonStates from './lib/button-states';
 import relationshipConfig from './lib/relationship-config';
+import setTokens from './lib/set-tokens';
 
 export default function (contextEl) {
 	personaliseLinks(contextEl);
@@ -11,5 +12,8 @@ export default function (contextEl) {
 			const subjectIds = relationships.map(item => item.uuid);
 			buttonStates.setStateOfManyButtons(relationshipName, subjectIds, true, contextEl);
 		}
+	});
+	setTokens({
+		container: contextEl
 	});
 }
