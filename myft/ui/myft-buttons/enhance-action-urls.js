77a130851b15df9598a1e305ffe70c3debf5a29b
chee <chee@snake.dog>
2018-08-10 15:19:42 +0100

    Only add set the action if there is one
     🐿 v2.9.0

--- myft/ui/myft-buttons/enhance-action-urls.js
+++ myft/ui/myft-buttons/enhance-action-urls.js
@@ -1,12 +1,12 @@
-
-
 export default function (context = document) {
 	['follow', 'save', 'instant']
 		.forEach(type => {
 			Array.from(context.querySelectorAll(`form.n-myft-ui--${type}`))
 				.forEach(form => {
+					const action = form.getAttribute('data-js-action');
+					if (!action) return;
 					form.setAttribute('method', 'POST');
-					form.setAttribute('action', form.getAttribute('data-js-action'));
+					form.setAttribute('action', action);
 				});
 		});
 }
