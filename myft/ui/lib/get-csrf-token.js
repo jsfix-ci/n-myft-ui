e5c58787017ddb879047a854691d22b34c72eb32
Glynn Phillips <524573+GlynnPhillips@users.noreply.github.com>
2019-02-22 14:39:35 +0000

    Trim the CSRF token to be the last 36 characters
    We should not put the whole session token into the DOM so we have opted for now to use the last 36 characters.

--- myft/ui/lib/get-csrf-token.js
+++ myft/ui/lib/get-csrf-token.js
@@ -1,3 +1,8 @@
 import Cookies from 'js-cookie';
+const desiredTokenLength = 36;
 
-module.exports = () => Cookies.get('FTSession_s') || Cookies.get('FTSession');
+module.exports = () => {
+  const token = Cookies.get('FTSession_s') || Cookies.get('FTSession');
+  const trimmedToken = token ? token.slice(-desiredTokenLength) : '';
+  return trimmedToken;
+};
