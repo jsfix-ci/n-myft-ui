b92a3c8d021d1f3397289325fe05f8a56c01268a
Alex Curtis <alex.curtis@ft.com>
2019-06-17 12:55:41 +0100

    Support Instant Alerts popup on x-dash.x-follow-button  🐿 v2.12.3

--- components/x-button-integration/index.js
+++ components/x-button-integration/index.js
@@ -1,18 +1,36 @@
 import nextMyftClient from 'next-myft-client';
+import {offerInstantAlerts} from '../../myft/ui/lib/push-notifications';
 
-const buttonEventHandler = ({ detail }) => {
-	const formData = {
-		token: detail.token
+const listenForFollowButtonClicks = (buttonEventName, pushNotificationsAreEnabled) => {
+	const buttonEventHandler = ({ detail }) => {
+		const formData = {
+			token: detail.token
+		};
+
+		nextMyftClient[detail.action](detail.actorType, detail.actorId, detail.relationshipName, detail.subjectType, detail.subjectId, formData)
+			.then( () => {
+				if( pushNotificationsAreEnabled ) {
+					if (detail.action === 'add' && detail.relationshipName === 'followed') {
+						offerInstantAlerts(detail.subjectId);
+					}
+				}
+			});
 	};
-
-	nextMyftClient[detail.action](detail.actorType, detail.actorId, detail.relationshipName, detail.subjectType, detail.subjectId, formData);
-};
-
-const listenForButtonClicks = (buttonEventName) => {
 	document.body.removeEventListener(buttonEventName, buttonEventHandler);
 	document.body.addEventListener(buttonEventName, buttonEventHandler);
 };
 
-export const initFollowButtons = () => listenForButtonClicks('x-follow-button');
+const listenForSaveButtonClicks = (buttonEventName) => {
+	const buttonEventHandler = ({ detail }) => {
+		const formData = {
+			token: detail.token
+		};
 
-export const initSaveButtons = () => listenForButtonClicks('x-article-save-button');
+		nextMyftClient[detail.action](detail.actorType, detail.actorId, detail.relationshipName, detail.subjectType, detail.subjectId, formData);
+	};
+	document.body.removeEventListener(buttonEventName, buttonEventHandler);
+	document.body.addEventListener(buttonEventName, buttonEventHandler);
+};
+
+export const initFollowButtons = (pushNotificationsAreEnabled=false) => listenForFollowButtonClicks('x-follow-button', pushNotificationsAreEnabled);
+export const initSaveButtons = () => listenForSaveButtonClicks('x-article-save-button');
