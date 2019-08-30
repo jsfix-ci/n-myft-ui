1c75cb06762a58b5882372ab2e080f9b02e3055f
Max Bladen-Clark <max@buzzard.dev>
2019-08-30 09:40:49 +0100

    Add myft header logo tooltip if: - user has myFT_HeaderTooltip flag enabled - user has navigated from a non FT domain
     🐿 v2.12.3

--- myft/ui/index.js
+++ myft/ui/index.js
@@ -2,10 +2,12 @@ import * as myFtButtons from './myft-buttons';
 import * as lists from './lists';
 import personaliseLinks from './personalise-links';
 import updateUi from './update-ui';
+import * as headerTooltip from '../../components/header-tooltip';
 
 function init (opts) {
 	myFtButtons.init(opts);
 	lists.init();
+	headerTooltip.init();
 }
 
 export {
