779a0939432310b9ec2c2591df51e392924281f1
Max Bladen-Clark <max@buzzard.dev>
2019-11-22 10:42:57 +0000

    fix error catching in update-count 🐿 v2.12.5

--- components/unread-articles-indicator/update-count.js
+++ components/unread-articles-indicator/update-count.js
@@ -6,37 +6,36 @@ import * as tracking from './tracking';
 import {UPDATE_INTERVAL, UPDATE_TIMEOUT} from './constants';
 
 
-function latest ( a, b ) {
-	if( !a ) {
-		return b;
-	}
-	if( !b ) {
-		return a;
-	}
-	return isAfter(parseISO(a),parseISO(b)) ? a : b;
+function latest (a, b) {
+	if (!a) return b;
+	if (!b) return a;
+	return isAfter(parseISO(a), parseISO(b)) ? a : b;
 }
 
 export default async function updateCount (userId, now) {
-	const lastUpdate = storage.getLastUpdate();
+	let doingUpdate;
+	try {
+		const lastUpdate = storage.getLastUpdate();
 
-	const isFirstUpdate = !lastUpdate; // Always update if there has never been an update before.
-	const noUpdateInProgress = !isFirstUpdate && (!lastUpdate.time || now.getTime() - lastUpdate.time.getTime() > UPDATE_INTERVAL);
-	const updateInProgressHasTimedOut = !isFirstUpdate && (!lastUpdate.updateStarted || now.getTime() - lastUpdate.updateStarted.getTime() > UPDATE_TIMEOUT);
+		const isFirstUpdate = !lastUpdate; // Always update if there has never been an update before.
+		const noUpdateInProgress = !isFirstUpdate && (!lastUpdate.time || now.getTime() - lastUpdate.time.getTime() > UPDATE_INTERVAL);
+		const updateInProgressHasTimedOut = !isFirstUpdate && (!lastUpdate.updateStarted || now.getTime() - lastUpdate.updateStarted.getTime() > UPDATE_TIMEOUT);
 
-	if (isFirstUpdate || noUpdateInProgress && updateInProgressHasTimedOut) {
+		if (isFirstUpdate || noUpdateInProgress && updateInProgressHasTimedOut) {
 
-		storage.updateLastUpdate({updateStarted: now});
+			storage.updateLastUpdate({updateStarted: now});
 
-		const startTime = latest(storage.getFeedStartTime(), storage.getIndicatorDismissedTime());
-		try {
+			const startTime = latest(storage.getFeedStartTime(), storage.getIndicatorDismissedTime());
+
+			doingUpdate = true;
 			const count = await countUnreadArticles(userId, startTime);
 			if (!lastUpdate || count !== lastUpdate.count) {
 				tracking.onCountChange(count, startTime);
 			}
 			storage.setLastUpdate({time: now, count, updateStarted: false});
-		} catch (error) {
-			storage.updateLastUpdate({updateStarted: false});
-			throw error;
 		}
+	} catch (error) {
+		if (doingUpdate) storage.updateLastUpdate({updateStarted: false});
+		throw error;
 	}
 };
