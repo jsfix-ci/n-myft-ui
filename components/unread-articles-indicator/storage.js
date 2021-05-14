c5b447f2f7fb7032f1b99373efc58a0f9c449cb3
Lily2point0 <lily.madar@ft.com>
2021-05-14 14:40:19 +0100

    remove unnecessary code, update tests, update readme

--- components/unread-articles-indicator/storage.js
+++ components/unread-articles-indicator/storage.js
@@ -2,10 +2,7 @@ import {isValid} from './date-fns';
 
 const DEVICE_SESSION_EXPIRY = 'deviceSessionExpiry';
 const FEED_START_TIME = 'newArticlesSinceTime';
-const LAST_INDICATOR_UPDATE = 'myFTIndicatorUpdate';
-const INDICATOR_DISMISSED_TIME = 'myFTIndicatorDismissedAt';
 
-const countChangeListeners = new Set();
 const isISOString = str => typeof str === 'string' && str.charAt(10) === 'T';
 const getStoredDate = key => {
 	const value = window.localStorage.getItem(key);
@@ -13,7 +10,6 @@ const getStoredDate = key => {
 
 	return isISOString(value) && isValid(date) ? date : null;
 };
-let lastCount;
 
 export const getDeviceSessionExpiry = () => getStoredDate(DEVICE_SESSION_EXPIRY);
 
@@ -22,10 +18,6 @@ export const setDeviceSessionExpiry = date => window.localStorage.setItem(DEVICE
 export const getFeedStartTime = () => getStoredDate(FEED_START_TIME);
 
 export const setFeedStartTime = date => window.localStorage.setItem(FEED_START_TIME, date.toISOString());
-
-export const getIndicatorDismissedTime = () => getStoredDate(INDICATOR_DISMISSED_TIME);
-
-export const setIndicatorDismissedTime = date => window.localStorage.setItem(INDICATOR_DISMISSED_TIME, date.toISOString());
 
 export const isAvailable = () => {
 	try {
@@ -39,39 +31,3 @@ export const isAvailable = () => {
 		return false;
 	}
 };
-
-
-export const setLastUpdate = (update = {}) => {
-	const toStore = Object.assign({}, update);
-	if (update.time) toStore.time = update.time.toISOString();
-	if (update.updateStarted) toStore.updateStarted = update.updateStarted.toISOString();
-	window.localStorage.setItem(LAST_INDICATOR_UPDATE, JSON.stringify(toStore));
-	fireListeners();
-};
-
-export const getLastUpdate = () => {
-	try {
-		const lastUpdate = JSON.parse(window.localStorage.getItem(LAST_INDICATOR_UPDATE));
-		if (lastUpdate.time) lastUpdate.time = new Date(lastUpdate.time);
-		if (lastUpdate.updateStarted) lastUpdate.updateStarted = new Date(lastUpdate.updateStarted);
-		return lastUpdate;
-	} catch (e) {}
-};
-
-export const updateLastUpdate = (update) => setLastUpdate(Object.assign({}, getLastUpdate(), update) );
-
-export const addCountChangeListeners = listener => {
-	if (!countChangeListeners.size) {
-		window.addEventListener('storage', fireListeners);
-	}
-	countChangeListeners.add(listener);
-};
-
-function fireListeners () {
-	const {count} = getLastUpdate() || {};
-	if (count === lastCount) return;
-	for (const listener of countChangeListeners) {
-		listener(count || 0);
-	}
-	lastCount = count || 0;
-}
