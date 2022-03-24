e12843ad144ab00a18597b9cb9fdbb3eec855e9e
Asuka.Ochi <asuka.ochi@ft.com>
2022-03-24 12:06:33 +0000

    Revert "moved test to same folder as component"
    This reverts commit 7e61bf4bbf11dc3770854f1c10c13c50f402a3c2.

--- components/follow-button/__tests__/follow-button.test.js
+++ components/follow-button/__tests__/follow-button.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FollowButton from './follow-button';
+import FollowButton from '../follow-button';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
