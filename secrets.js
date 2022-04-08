8cb6cd059f395628a0f64c1159aeda460e192cbf
Emilio de la Torre <emilio.delatorre@ft.com>
2022-04-08 17:07:14 +0100

    Use o-errors to catch and report errors on the save article to list variant
    Initialises o-errors on the save article variant to report errors to next-article's Sentry project.
    Adds try/catch structures to the showSaveArticleToListVariant function and all its components' event listeners. These structures catch the errors and enrich them with component property with value professorList. We'll use this property to create a custom alert for errors coming from the variant

--- secrets.js
+++ secrets.js
@@ -5,6 +5,7 @@ module.exports = {
 		'11df4800-391a-11ea-973b-4a52933561ab', // components/unread-articles-indicator/README.md:67
 		'190b4443-dc03-bd53-e79b-b4b6fbd04e64', // segment ID for subscribe URL
 		'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx', // regex for uuid generator
-		'a5676e20-5c92-47f3-a76c-11f9761121f5' // test/navigationAlphaTest.spec.js
+		'a5676e20-5c92-47f3-a76c-11f9761121f5', // test/navigationAlphaTest.spec.js
+		'https://d63d3b29fd374c3d95fa814a38c9bac9@o16684.ingest.sentry.io/79480', // Sentry URL myft/ui/save-article-to-list-variant:13
 	]
 };
