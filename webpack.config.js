78403b5d0bda79f32e7980cf1cd641cd5152e0c1
Dawn Budge <dawn.budge@ft.com>
2022-05-13 13:45:37 +0100

    Add missing webpack file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { PageKitJsPlugin } = require('@financial-times/dotcom-build-js');
+const { PageKitSassPlugin } = require('@financial-times/dotcom-build-sass');
+const { PageKitBowerResolvePlugin } = require('@financial-times/dotcom-build-bower-resolve');
+
+module.exports = {
+	plugins: [
+		new PageKitJsPlugin(),
+		new PageKitSassPlugin(),
+		new PageKitBowerResolvePlugin()
+	],
+	entry: {
+		scripts:  [
+			'./myft/index.js',
+			'./myft-common/index.js'
+		],
+		
+		styles: './myft/test.scss'
+	},
+	output: {
+		path: path.resolve(__dirname, 'public')
+	},
+};
