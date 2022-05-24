e78f08ca422489c1de6ede6877bbfdde807c75e4
Dawn Budge <dawn.budge@ft.com>
2022-05-24 19:00:27 +0100

    Update o-errors to v5

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ const { PageKitSassPlugin } = require('@financial-times/dotcom-build-sass');
 module.exports = {
 	plugins: [
 		new PageKitJsPlugin(),
-		new PageKitSassPlugin({
-			includePaths: [path.resolve('./node_modules/@financial-times')],
-		}),
+		new PageKitSassPlugin()
 	],
 	entry: {
 		scripts: ['./myft/index.js', './myft-common/index.js'],
