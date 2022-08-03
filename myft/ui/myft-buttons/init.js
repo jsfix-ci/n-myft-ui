0fa34cabed4950486479e278808efa33bc0ab2b4
James Nicholls <james.nicholls@ft.com>
2022-08-03 11:12:25 +0100

    Stop firing myFT tracking events for list-related actions
    List-related actions will now use a `list` category

--- myft/ui/myft-buttons/init.js
+++ myft/ui/myft-buttons/init.js
@@ -65,6 +65,14 @@ function signedInEventListeners () {
 						const resultData = event.detail.results && event.detail.results[0];
 						const isPressed = !!event.detail.results;
 						buttonStates.setStateOfButton(relationshipName, event.detail.subject, isPressed, undefined, resultData, true);
+
+						// don't send tracking events for list actions, as any
+						// list-related code will send its own `list` category
+						// tracking events
+						if (actorType === 'list') {
+							return;
+						}
+
 						tracking.custom({
 							subjectType,
 							action,
